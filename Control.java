575d6312eaad6276c3ff8d3a9678ae03221732f9
dairayi <72094446+dairayi@users.noreply.github.com>
2022-04-04 22:59:53 +0100

    Update Control.java

--- Control.java
+++ Control.java
@@ -5,8 +5,11 @@ import java.io.FileNotFoundException;
 public class Control {
     public static void main(String[] args) throws FileNotFoundException {
 
-        Class1 r = new Class1("MLdata.csv");
-        System.out.println(r.calc_probs());
+        r.gathering_data();
+        //r.calc_probs();
+
+        r.test("female","no","no","rural","no");
+
 
         //r.addnewdata();
         
