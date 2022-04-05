4ae2c961b27fec95fe60467067e23c9e7b8d5530
dairayi <72094446+dairayi@users.noreply.github.com>
2022-04-05 23:21:42 +0100

    Update Class1.java

--- Class1.java
+++ Class1.java
@@ -101,25 +101,125 @@ public class Class1 {
             ioe.printStackTrace();
         }
 
-        System.out.println(Arrays.toString(gender));
-        System.out.println(Arrays.toString(parent));
-        System.out.println(Arrays.toString(part_time));
-        System.out.println(Arrays.toString(urban_rural));
-        System.out.println(Arrays.toString(business));
-        System.out.println(Arrays.toString(entrepreneur));
-        System.out.println(gender[5]);
+    }
 
+    public void count(){
+
+        //countMap holds the count details of each element
+        Map<String, String> countgender = new HashMap<String, String>();
+        for (String key : gender) {
+            if (countgender.containsKey(key)) {
+                int count = Integer.parseInt(countgender.get(key));
+                count++;
+                countgender.put(key, String.valueOf(count));
+            } else {
+                countgender.put(key, String.valueOf(1));
+            }
+        }
+
+        System.out.println(countgender);
+
+
+        Map<String, String> countparent = new HashMap<String, String>();
+        for (String key : parent) {
+            if (countparent.containsKey(key)) {
+                int count = Integer.parseInt(countparent.get(key));
+                count++;
+                countparent.put(key, String.valueOf(count));
+            } else {
+                countparent.put(key, String.valueOf(1));
+            }
+        }
+
+        System.out.println(countparent);
+
+
+        Map<String, String> countpart_time = new HashMap<String, String>();
+        for (String key : part_time) {
+            if (countpart_time.containsKey(key)) {
+                int count = Integer.parseInt(countpart_time.get(key));
+                count++;
+                countpart_time.put(key, String.valueOf(count));
+            } else {
+                countpart_time.put(key, String.valueOf(1));
+            }
+        }
+
+        System.out.println(countpart_time.get("Yes"));
     }
 
     public void calc_probs(){
-        
+
 
 
     }
 
 
-    public void run(String gender,String parent,String parttime,String living,String business){
+    public void run(String gender,String parent,String parttime,String living,String business) {
 
+        float y;
+
+        int x = 0;
+
+        float y1;
+        float y2;
+        float y3;
+        float y4;
+        float y5;
+
+        float x1;
+        float x2;
+        float x3;
+        float x4;
+        float x5;
+
+        int count = 0;
+        if (Objects.equals(gender, "female")) {
+
+        }
+        /*
+        else{
+            x1=a2/a11;
+            y1=a2/gender_total;
+        }
+
+        if(Objects.equals(parent, "yes")){
+            x2=a3/a11;
+            y2=a3/parent_total;
+        }
+        else{
+            x2=a4/a11;
+            y2=a4/parent_total;
+        }
+
+        if(Objects.equals(parttime, "yes")){
+            x3=a5/a11;
+            y3=a5/parttime_total;
+        }
+        else{
+            x3=a6/a11;
+            y3=a6/parttime_total;
+        }
+
+        if(Objects.equals(living, "rural")){
+            x4=a7/a11;
+            y4=a7/urbanrural_total;
+        }
+        else{
+            x4=a8/a11;
+            y4=a8/urbanrural_total;
+        }
+
+        if(Objects.equals(business, "yes")){
+            x5=a9/a11;
+            y5=a9/business_total;
+        }
+        else{
+            x5=a10/a11;
+            y5=a10/business_total;
+        }
+
+         */
 
     }
 /*
@@ -142,5 +242,4 @@ public class Class1 {
     }
 */
 }
-
 
