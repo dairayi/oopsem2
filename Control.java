7e0fa583cfd6866ddce7fff6741c5b6e46aea858
dairayi <72094446+dairayi@users.noreply.github.com>
2022-04-14 18:42:49 +0100

    Update Control.java

--- Control.java
+++ Control.java
@@ -5,13 +5,7 @@ import java.io.FileNotFoundException;
 public class Control {
     public static void main(String[] args) throws FileNotFoundException {
 
-        r.gathering_data();
-        //r.calc_probs();
-
-        r.test("female","no","no","rural","no");
-
-
-        //r.addnewdata();
+        Class2 myScreen = new Class2("class2");
         
     }
 }
