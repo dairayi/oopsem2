1a6d887eed1ce8dd3d72ee148866d3e0d8563836
dairayi <72094446+dairayi@users.noreply.github.com>
2022-04-15 17:13:25 +0100

    Update Control.java

--- Control.java
+++ Control.java
@@ -1,11 +1,11 @@
-package assignment1;
+package Assignment1;
 
 import java.io.FileNotFoundException;
 
 public class Control {
     public static void main(String[] args) throws FileNotFoundException {
 
-        Class2 myScreen = new Class2("class2");
+        MyGUI myScreen = new MyGUI("MyGUI");
         
     }
 }
